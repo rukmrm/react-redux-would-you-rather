71f97bc43287023f60defc81a854438ff9ca750e
rukmrm <areyoukittenmerightmiaow@gmail.com>
2020-01-24 14:32:37 -0800

    feat: Add utils/helper.js formatQuestion2

---
+++ src/utils/helpers.js
@@ -0,0 +1,22 @@
+export function formatDate(timestamp) {
+  const d = new Date(timestamp)
+  const time = d.toLocaleTimeString('en-US')
+  return time.substr(0, 5) + time.slice(-2) + ' | ' + d.toLocaleDateString()
+}
+
+export function formatQuestion2(question) {
+  console.log(question)
+  const { id, optionOneText, optionTwoText } = question
+
+  return {
+    id,
+    optionOne: {
+      votes: [],
+      text: optionOneText,
+    },
+    optionTwo: {
+      votes: [],
+      text: optionTwoText,
+    },
+  }
+}

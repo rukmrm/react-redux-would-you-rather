22f5d3dff64d82d2db86c8fbb04fc16571b2d069
rukmrm <areyoukittenmerightmiaow@gmail.com>
2020-01-24 16:40:02 -0800

    feat: Add author to Question display

--- src/components/Question.js
+++ src/components/Question.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import Avatar from './Avatar'
 import { formatQuestion2 } from '../utils/helpers'
 import { connect } from 'react-redux'
+import { Item } from 'semantic-ui-react'
 
 class Question extends Component {
   render() {
@@ -9,16 +10,21 @@ class Question extends Component {
     console.log(question)
 
     return (
-      <div className="temp">
-        QuestionTeaser
-        <div className="question-teaser-header">user asks:</div>
-        <div className="question-teaser-body">
-          <Avatar />
-          <div className="temp">
-            <h5>Would you rather...</h5>
-            <div className="teaser-text">{question.optionOne.text}</div>
-            <div className="teaser-text">{question.optionTwo.text}</div>
-            <button>View Poll</button>
+      <div className="list-item-content">
+        <div>
+          <div className="question-teaser-header">{question.author} asks:</div>
+          <div className="question-teaser-body">
+            <Avatar />
+            <div className="circle-medium">
+              <img className="cropped" src="https://placekitten.com/100/100" />
+            </div>
+
+            <div className="temp">
+              <h5>Would you rather...</h5>
+              <div className="teaser-text">{question.optionOne.text}</div>
+              <div className="teaser-text">{question.optionTwo.text}</div>
+              <button>View Poll</button>
+            </div>
           </div>
         </div>
       </div>
@@ -28,10 +34,6 @@ class Question extends Component {
 
 function mapStateToProps({ users, questions }, { id }) {
   const question = questions[id]
-  console.log('mapStateToProps', users)
-  console.log('mapStateToProps', questions)
-  console.log('mapStateToProps', id)
-  console.log('mapStateToProps', question)
 
   return {
     // question: question ? formatQuestion2(question) : null,
